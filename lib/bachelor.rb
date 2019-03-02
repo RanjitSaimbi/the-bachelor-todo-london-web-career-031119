5781bae75f6a83a21d74e402bb42feccf301e901
Ranjit Saimbi <ranjit.saimbi@gmail.com>
2019-03-02 16:19:04 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,9 +1,29 @@
+require 'pry'
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  celeb = ""
+  data.each do |season_number, data_array|
+    if season_number == season 
+    data_array.each do |person|
+      if person["status"] == "Winner"
+        celeb = person["name"]
+    end 
+    end 
+  end 
+end 
+celeb.split.first
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  celeb = ""
+  data.each do |season_number, data_array|
+    data_array.each do |person|
+      if person["occupation"] == occupation
+        celeb = person["name"]
+      end 
+    end 
+  end
+  celeb
 end
 
 def count_contestants_by_hometown(data, hometown)
