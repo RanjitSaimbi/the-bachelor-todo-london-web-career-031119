50dd5c3f3d7560a53ac86b04c8c5f507bcba5907
Ranjit Saimbi <ranjit.saimbi@gmail.com>
2019-03-02 16:33:10 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -56,7 +56,7 @@ def get_average_age_for_season(data, season)
   data.each do |season_number, data_array|
     if season_number == season 
       data_array.each do |person|
-        array_of_ages << person["age"]
+        array_of_ages << person["age"].to_i
       end 
     end 
   end 
