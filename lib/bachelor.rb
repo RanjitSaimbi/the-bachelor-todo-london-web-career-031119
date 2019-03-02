c26c9c5a641313253fc385ff271e56b6d4984685
Ranjit Saimbi <ranjit.saimbi@gmail.com>
2019-03-02 16:22:11 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -31,7 +31,7 @@ def count_contestants_by_hometown(data, hometown)
   data.each do |season_number, data_array|
     data_array.each do |person|
       if person["hometown"] == hometown
-        
+        array_of_contestants << person["name"]
       end 
     end 
   end 
